a6d44f98f391f694af126b04357877314d76780d
Christian <cholland2408@gmail.com>
2019-07-09 15:39:35 +0200

    Addem geom_rugs for (measured) cell via. histograms

--- ShinyCellDeathSignatures/sub/02_server_explore_measured_data.R
+++ ShinyCellDeathSignatures/sub/02_server_explore_measured_data.R
@@ -21,8 +21,11 @@ output$measured_df = DT::renderDataTable({
   }
 })
 
-measured_df_filtered = observeEvent(input$measured_df_state, {
-  input$measured_df_search_columns
+measured_df_filtered = eventReactive(input$measured_df_state, {
+  s = input$measured_df_search_columns %>% 
+    na_if("") %>%
+    parse_column_filtering()
+  return(s)
 })
 
 output$download_achilles_measured = downloadHandler(
@@ -46,23 +49,68 @@ output$download_ctrp_measured = downloadHandler(
 # histogram showing the measured cell viabilities for either achilles or ctrp
 output$histogram_measured = renderPlotly({
   if (selected_resource_measured() == "achilles") {
-    p = achilles_measured %>%
-      ggplot(aes(x=shRNA_abundance)) +
-      geom_histogram(color="white") +
-      my_theme() +
-      labs(x="shRNA abundance", y="Count")
+    print(measured_df_filtered())
+    data_rug = achilles_measured %>%
+      filter(if (is.na(measured_df_filtered()[1])) TRUE else sig_id %in% measured_df_filtered()[1]) %>%
+      filter(if (is.na(measured_df_filtered()[2])) TRUE else pert_id %in% measured_df_filtered()[2]) %>%
+      filter(if (is.na(measured_df_filtered()[3])) TRUE else pert_iname %in% measured_df_filtered()[3]) %>%
+      filter(if (is.na(measured_df_filtered()[4])) TRUE else cell_id %in% measured_df_filtered()[4]) %>%
+      filter(if (is.na(measured_df_filtered()[5])) TRUE else pert_itime %in% measured_df_filtered()[5]) %>%
+      filter(if (is.na(measured_df_filtered()[6])) TRUE else between(shRNA_abundance, as.numeric(measured_df_filtered()[6]), as.numeric(measured_df_filtered()[7])))
+    
+
+    if (nrow(data_rug) <= 200) {
+      print("hi")
+      p = achilles_measured %>%
+        ggplot(aes(x=shRNA_abundance)) +
+        geom_histogram(color="white") +
+        my_theme() +
+        labs(x="shRNA abundance", y="Count") +
+        geom_rug(data = data_rug, color="red")
+        
+    } else {
+      p = achilles_measured %>%
+        ggplot(aes(x=shRNA_abundance)) +
+        geom_histogram(color="white") +
+        my_theme() +
+        labs(x="shRNA abundance", y="Count")
+    }
   } else if (selected_resource_measured() == "ctrp") {
-    p = ctrp_measured %>% 
-      ggplot(aes(x=cell_viability)) +
-      geom_histogram(color = "white", boundary = 0) +
-      my_theme() +
-      labs(x="shRNA abundance", y="Count")
+    print(measured_df_filtered())
+    data_rug = ctrp_measured %>%
+      filter(if (is.na(measured_df_filtered()[1])) TRUE else sig_id %in% measured_df_filtered()[1]) %>%
+      filter(if (is.na(measured_df_filtered()[2])) TRUE else pert_id %in% measured_df_filtered()[2]) %>%
+      filter(if (is.na(measured_df_filtered()[3])) TRUE else pert_iname %in% measured_df_filtered()[3]) %>%
+      filter(if (is.na(measured_df_filtered()[4])) TRUE else cell_id %in% measured_df_filtered()[4]) %>%
+      filter(if (is.na(measured_df_filtered()[5])) TRUE else pert_itime %in% measured_df_filtered()[5]) %>%
+      filter(if (is.na(measured_df_filtered()[6])) TRUE else between(log10_conc_uM_LINCS, as.numeric(measured_df_filtered()[6]), as.numeric(measured_df_filtered()[7]))) %>%
+      filter(if (is.na(measured_df_filtered()[8])) TRUE else between(log10_conc_uM_CTRP, as.numeric(measured_df_filtered()[8]), as.numeric(measured_df_filtered()[9]))) %>%
+      filter(if (is.na(measured_df_filtered()[10])) TRUE else between(cell_viability, as.numeric(measured_df_filtered()[10]), as.numeric(measured_df_filtered()[11])))
+    
+    if (nrow(data_rug) <= 200) {
+      print("hi2")
+      p = ctrp_measured %>%
+        ggplot(aes(x=cell_viability)) +
+        geom_histogram(color="white") +
+        my_theme() +
+        labs(x="Cell Viability", y="Count") +
+        geom_rug(data = data_rug, color="red")
+      
+    } else {
+      p = ctrp_measured %>%
+        ggplot(aes(x=cell_viability)) +
+        geom_histogram(color="white") +
+        my_theme() +
+        labs(x="Cell Viability", y="Count")
+    }
   }
+  # return(p)
 })
 
 # pie chart displaying cell lines 
 output$pie_measured_cellline = renderPlotly({
   if (selected_resource_measured() == "achilles") {
+
     achilles_measured %>%
       transmute(cell_id = fct_lump(cell_id)) %>%
       count(cell_id) %>%
