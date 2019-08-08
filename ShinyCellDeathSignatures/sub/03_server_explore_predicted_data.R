fb61f455e4a36830dd7af936293db9a9b8a6a8af
Christian <cholland2408@gmail.com>
2019-08-08 15:01:59 +0200

    Split large data set into multiple smaller ones

--- ShinyCellDeathSignatures/sub/03_server_explore_predicted_data.R
+++ ShinyCellDeathSignatures/sub/03_server_explore_predicted_data.R
@@ -1,27 +1,54 @@
-# selected_resource_predicted = eventReactive(
-#   input$select_resource_predicted, {
-#     input$select_resource_predicted
-#   }
-# )
+selected_pert_predicted = eventReactive(
+  input$select_pert_predicted, {
+    switch(
+      input$select_pert_predicted,
+      "cl" = {
+        # rm(cl_df, crispr_df, oe_df, shrna_df, ctrl_df)
+        cl_df = read_csv("data/predictions/compound_ligand_pred.csv",)
+        },
+      "crispr" = {
+        # rm(cl_df, crispr_df, oe_df, shrna_df, ctrl_df)
+        crispr_df = read_csv("data/predictions/crispr_pred.csv")
+        }, 
+      "oe" = {
+        # rm(cl_df, crispr_df, oe_df, shrna_df, ctrl_df)
+        oe_df = read_csv("data/predictions/over_expression_pred.csv")
+        },
+      "shrna" = {
+        # rm(cl_df, crispr_df, oe_df, shrna_df, ctrl_df)
+        shrna_df = read_csv("data/predictions/shRNA_pred.csv")
+        },
+      "ctrl" = {
+        # rm(cl_df, crispr_df, oe_df, shrna_df, ctrl_df)
+        ctrl_df = read_csv("data/predictions/control_pred.csv")
+      }
+      )
+  }
+)
 
 output$predicted_df = DT::renderDataTable({
-  pred %>%
+  selected_pert_predicted() %>%
+    mutate_if(is.character, as.factor) %>%
+    mutate(pert_itime = ordered(pert_itime,
+                                levels = c("1 h","2 h", "3 h", "4 h", "6 h",
+                                           "24 h", "48 h", "72 h", "96 h",
+                                           "120 h", "144 h", "168 h"))) %>%
     mutate_if(is.double, signif, 3) %>%
     DT::datatable(., escape = F, option = list(scrollX = TRUE, autoWidth=T), 
                   filter = "top", selection = list(target = "none"))
 })
 
-output$download_predicted_achilles_ctrp = downloadHandler(
+output$download_select_pert = downloadHandler(
   filename = function() {
-    "predicted_achilles_ctrp.csv"
+    "selected_viabilities_achilles_ctrp.csv"
   },
   content = function(file) {
-    pred %>%
+    selected_pert_predicted() %>%
       write_csv(., file)
   })
 
 output$histogram_predicted = renderPlotly({
-  pred %>%
+  selected_pert_predicted() %>%
     gather(resource, viability, Achilles_prediction, CTRP_prediction) %>%
     mutate(resource = str_remove(string = resource,pattern = "_prediction")) %>%
     ggplot(aes(x=viability)) +
@@ -31,3 +58,4 @@ output$histogram_predicted = renderPlotly({
     theme(legend.position = "none") +
     labs(x="Viability", y="Count")
 })
+
